047fc261ec84b6a07106bc73f890a4a13a08e3cf
PanruifengWawa <46937922@qq.com>
2017-02-17 17:04:15 +0800

    doc

---
+++ doc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "接口文档",  "version": "0.1.0",  "description": "后端接口文档",  "title": "后端接口文档",  "url": "https://123.56.220.72:8080/Student/",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-02-17T08:53:16.238Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
