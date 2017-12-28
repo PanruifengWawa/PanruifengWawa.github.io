2e64ae03306345065690520468660e3de98c161b
PanruifengWawa <46937922@qq.com>
2017-12-28 21:21:39 +0800

    apidoc adv

---
+++ doc/adv/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "接口文档",  "version": "0.1.0",  "description": "后端接口文档",  "title": "后端接口文档",  "url": "http://xxxx:8080/adv/",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-12-28T13:19:32.054Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
