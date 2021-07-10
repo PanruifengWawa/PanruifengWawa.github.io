7f0e559811febccd37991f87bdccd42255b18eac
panruifeng <prf@panruifeng.com>
2021-07-11 02:40:31 +0800

    gzf

---
+++ doc/gzf/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "接口文档",
+  "version": "0.1.0",
+  "description": "后端接口文档",
+  "title": "后端接口文档",
+  "url": "http://localhost:8080/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-07-10T18:38:21.525Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
